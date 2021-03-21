a548941979f8806f5d93400b8835d5418d82cc63
Adabi Raihan Muhammad <adabi005@yahoo.co.id>
2021-03-21 21:32:12 +0700

    Update ViewHome.php

--- resources/views/ViewHome.php
+++ resources/views/ViewHome.php
@@ -7,6 +7,15 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Internsip</title>
 
+<!--RESOURCES BUAT MODAL-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!--BRRRRRRRRRRRRRRRT-->
+
 <style>
 
     #title{
@@ -26,7 +35,7 @@
     #nav2{
         font-size: 1.5vw;
         position: relative;
-        right: -115%;
+        right: -70%;
         bottom:-15%;
         color: black;
         font-weight:bold;
@@ -35,7 +44,7 @@
     #nav3{
         font-size: 1.5vw;
         position: relative;
-        right: -150%;
+        right: -120%;
         bottom:-15%;
         color: black;
         font-weight:bold;
@@ -107,6 +116,78 @@
         height: 50px;
         width: 260px;
     }
+
+    .modal-login {
+        color: #636363;
+        width: 350px;
+    }
+    .modal-login .modal-content {
+        padding: 20px;
+        border-radius: 5px;
+        border: none;
+    }
+    .modal-login .modal-header {
+        border-bottom: none;
+        position: relative;
+        justify-content: center;
+    }
+    .modal-login h4 {
+        text-align: center;
+        font-size: 26px;
+    }
+    .modal-login  .form-group {
+        position: relative;
+    }
+    .modal-login i {
+        position: absolute;
+        left: 13px;
+        top: 11px;
+        font-size: 18px;
+    }
+    .modal-login .form-control {
+        padding-left: 40px;
+    }
+    .modal-login .form-control:focus {
+        border-color: #00ce81;
+    }
+    .modal-login .form-control, .modal-login .btn {
+        min-height: 40px;
+        border-radius: 3px; 
+    }
+    .modal-login .hint-text {
+        text-align: center;
+        padding-top: 10px;
+    }
+    .modal-login .close {
+        position: absolute;
+        top: -5px;
+        right: -5px;
+    }
+    .modal-login .btn, .modal-login .btn:active {	
+        border: none;
+        background: #00ce81 !important;
+        line-height: normal;
+    }
+    .modal-login .btn:hover, .modal-login .btn:focus {
+        background: #00bf78 !important;
+    }
+    .modal-login .modal-footer {
+        background: #ecf0f1;
+        border-color: #dee4e7;
+        text-align: center;
+        margin: 0 -20px -20px;
+        border-radius: 5px;
+        font-size: 13px;
+        justify-content: center;
+    }
+    .modal-login .modal-footer a {
+        color: #999;
+    }
+
+    #modal-body-regist{
+        width: 500px;
+    }
+
 </style>
 
 </head>
@@ -123,10 +204,10 @@
           <a class="nav-link active" id="nav1" href="#">Cari Lowongan</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" id="nav2" href="#">Tips Diterima</a>
+          <a class="nav-link" id="nav2" href="#">Cari Perusahaan</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" id="nav3" href="#">Cari Perusahaan</a>
+          <a href="#ModalLogin" class="nav-link active trigger-btn" data-toggle="modal" id="nav3">My Internsip</a>
         </li>
         <li>
             <div class="language-picker js-language-picker">
@@ -164,5 +245,94 @@
             </form>
         </div>
 </div>
+
+<!-- MODAL LOGIN POP UP-->
+
+<div id="ModalLogin" class="modal fade">
+	<div class="modal-dialog modal-login">
+		<div class="modal-content">
+			<div class="modal-header">				
+				<h4 class="modal-title">Member Login</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			</div>
+			<div class="modal-body">
+				<form action="#" method="post">
+					<div class="form-group">
+						<i class="fa fa-user"></i>
+						<input type="text" class="form-control" placeholder="Username" required="required">
+					</div>
+					<div class="form-group">
+						<i class="fa fa-lock"></i>
+						<input type="password" class="form-control" placeholder="Password" required="required">					
+					</div>
+					<div class="form-group">
+						<input type="submit" class="btn btn-primary btn-block btn-lg" value="Login">
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<a href="#ModalRegister" class="nav-link active trigger-btn" data-dismiss="modal" data-toggle="modal">Belum punya akun ? Klik untuk Registrasi</a>
+			</div>
+		</div>
+	</div>
+</div>    
+<!--MODAL BRTTTTTTT--> 
+
+
+<!-- Modal Register HTML-->
+<div id="ModalRegister" class="modal fade" >
+	<div class="modal-dialog modal-login" id="modal-body-regist">
+		<div class="modal-content">
+			<div class="modal-header">				
+				<h4 class="modal-title">Registration</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			</div>
+			<div class="modal-body">
+				<form action="#" method="post">
+					<div class="form-group">
+						<i class="fa fa-user"></i>
+						<input type="text" class="form-control" placeholder="Username" required="required">
+					</div>
+                    <div class="form-group">
+						<i class="fa fa-lock"></i>
+						<input type="password" class="form-control" placeholder="Password" required="required">
+					</div>
+                    <div class="form-group">
+						<i class="fa fa-lock"></i>
+						<input type="password" class="form-control" placeholder="Confirm Password" required="required">
+					</div>
+                    <div class="form-group">
+						<i class="fa fa-address-card"></i>
+						<input type="text" class="form-control" placeholder="Nama Lengkap" required="required">
+					</div>
+					<div class="form-group">
+						<i class="fa fa-calendar "></i>
+						<input type="text" class="form-control" placeholder="TTL" required="required">					
+					</div>
+                    <div class="form-group">
+						<i class="fa fa-university"></i>
+						<input type="text" class="form-control" placeholder="Pendidikan Terakhir" required="required">					
+					</div>
+                    <div class="form-group">
+						<i class="fa fa-envelope "></i>
+						<input type="text" class="form-control" placeholder="Email" required="required">					
+					</div>
+                    <div class="form-group">
+						<i class="fa fa-address-book "></i>
+						<input type="text" class="form-control" placeholder="No Telp" required="required">					
+					</div>
+                    <div class="form-group">
+						<i class="fa fa-home"></i>
+						<input type="text" class="form-control" placeholder="Alamat" required="required">					
+					</div>
+					<div class="form-group">
+						<input type="submit" class="btn btn-primary btn-block btn-lg" value="Login">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>    
+
 </body>
 </html>
